a9211fe0853d722ca9e1b9d584c1da893226972d
Kieran Elliott <kierse@pissiphany.com>
2018-11-14 15:16:46 -0800

    refactored to introduce method for opening a dispute

--- contracts/CarShareRegistry.sol
+++ contracts/CarShareRegistry.sol
@@ -1,6 +1,6 @@
 contract CarShareRegistry {
     enum CarState {
-        AVAILABLE, UNAVAILABLE, RENTED, IN_DISPUTE, IS_DAMAGED
+        AVAILABLE, UNAVAILABLE, RENTED, IN_DISPUTE
     }
     
     struct Car {
@@ -17,7 +17,18 @@ contract CarShareRegistry {
         uint256 timestamp;
         string vin;
         address renter;
-        uint8 cleanliness; // 1- 10, 1 = terrible, 10 = excellent
+        uint8 cleanliness; // 1 - 10, 1 = terrible, 10 = excellent
+        string[] images;   // list of urls pointing to images documenting any/all 
+                           // problems with car
+    }
+    
+    struct Dispute {
+        uint256 timestamp;
+        string vin;
+        address renter;
+        bool refundRequested;
+        bool carIsDamaged; 
+        uint8 cleanliness; // 1 - 10, 1 = terrible, 10 = excellent
         string[] images;   // list of urls pointing to images documenting any/all 
                            // problems with car
     }
@@ -41,9 +52,12 @@ contract CarShareRegistry {
     // vin => rental_total
     mapping(string => InProgessTransaction) private pendingTransactions;
 
-    // open CarFeedback reports
+    // unretrieved CarFeedback reports
     // vin => CarFeedback[]
     mapping(string => CarFeedback[]) openFeedback;
+    
+    // ongoing Dispute reports
+    mapping(string => Dispute) ongoingDisputes;
 
     // register new car
     function registerCar(string vin, uint8 costPerHour, CarState state) public {
@@ -66,13 +80,13 @@ contract CarShareRegistry {
          *  6) Transfer funds from renter to contract
          *   6a) Throw exception if transfer fails
          *  7) Create new Transaction instance and add to pendingTransactions
-         *  8) return vin, lat, long
+         *  8) return (vin, lat, long)
          * 
          **/
     }
     
-    function providePreTripFeedback(
-        string vin, uint8 cleanliness, string[] images, bool disputeRental
+    function provideTripFeedback(
+        string vin, uint8 cleanliness, string[] images
     ) public {
         /**
          * We have a few things to do here:
@@ -84,7 +98,30 @@ contract CarShareRegistry {
          *  3) Verify that images contain urls
          *   3a) Throw exception if any images don't start with http[s]:// (necessary ???)
          *  4) Create new CarFeedback and add to openFeedback mapping
-         *  5) Set Car.state = IN_DISPUTE if disputeRental is true
+         * 
+         **/
+    }
+    
+    function openDispute(
+        string vin, 
+        bool refundRequested, 
+        bool carIsDamaged, 
+        uint8 cleanliness, 
+        string[] images
+    ) public (bool) {
+        /**
+         * We have a few things to do here:
+         * 
+         *  1) Retrieve the Car struct using the indicated vin
+         *   1a) Throw exception if vin does NOT map to a Car struct
+         *  2) Verify that the indicated renter has rented the Car
+         *   2a) Throw exception if the renter has NOT rented the Car
+         *  3) Verify that renter indicated a valid cleanliness value
+         *   3a) Throw exception if 1 > cleanliness > 10
+         *  4) Verify that images contain urls
+         *   4a) Throw exception if any images don't start with http[s]:// (necessary ???)
+         *  5) Create new Dispute and add to ongoingDisputes mapping
+         *  6) Set Car.state = IN_DISPUTE
          * 
          **/
     }
@@ -95,19 +132,16 @@ contract CarShareRegistry {
          * 
          *  1) Retrieve the Car struct using the indicated vin
          *   1a) Return false if the vin does NOT map to a Car struct
-         *  2) Verify that the indicated renter has rented the Car
-         *   2a) Return false if the renter has not rented the Car
-         *  3) return true
+         *  2) Verify that the Car is rented
+         *   2a) Throw exception if Car.state != CarState.RENTED
+         *  3) Verify that the indicated renter has rented the Car
+         *   3a) Return false if the renter has not rented the Car
+         *  4) return true
          * 
          **/
     }
     
     function returnCar(string vin, uint8 lat, uint8 long,  string[] images) public (bool) {
-        // app get gas level and gps from car (bluetooth)
-        // app checks current gps vs expected and shows error or makes call to end trip
-        // 
-        // transfer money from contract to owner
-        // update Car struct
         /**
          * We have a few things to do here:
          * 
@@ -117,8 +151,9 @@ contract CarShareRegistry {
          *   2a) Throw exception if Car's state != CarState.RENTED
          *  3) Verify that Car is rented to message sender
          *   3a) Throw exception if msg.sender != Car.renter
-         *  x) Transfer funds from contract to owner
-         *   xa) Throw exception if transfer fails
+         *  4) Transfer funds from contract to owner
+         *   4a) Throw exception if transfer fails
+         *  5) return true
          * 
          **/
     }
